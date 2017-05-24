317f93ea03321bd4139e720c81ca731408cd6fc0
palomahnhp <hernandezp@madrid.es>
2017-05-24 18:59:51 +0200

    WIP - prueba para borrar

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -22,9 +22,9 @@ WickedPdf.config = {
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
   wkhtmltopdf: exe_path,
-  exe_path: exe_path
+  exe_path: exe_path,
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html.erb',
+   layout: 'layouts/pdf.html.erb',
 }
